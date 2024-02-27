609e6247cb8f911a5e19da0b9f773aec5b5d15b9
bmit-atm <martin.atanasov@espas.ch>
2024-02-27 21:51:47 +0100

    added some changes

---
+++ app/Http/Controllers/SignatureController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+class SignatureController extends Controller
+{
+    
+    public function generate(Request $request)
+{
+    try {
+        // Benutzername aus dem Request abrufen
+        $username = $request->input('username');
+
+        // Hochgeladenes Bild verarbeiten
+        if ($request->hasFile('file')) {
+            $image = $request->file('file');
+            $imagePath = $image->store('logos', 'public');
+
+            // HTML-Datei für die Outlook-Signatur generieren
+            $htmlContent = "<html><body>";
+            $htmlContent .= "<h1>Outlook-Signatur für $username</h1>";
+            $htmlContent .= "<p>Benutzername: $username</p>";
+            $htmlContent .= "<img src='" . Storage::url($imagePath) . "' alt='Logo'>";
+            $htmlContent .= "</body></html>";
+
+            // Datei als Antwort senden
+            return response($htmlContent)
+                ->header('Content-Type', 'text/html')
+                ->header('Content-Disposition', 'attachment; filename="signature.htm"');
+        } else {
+            return response()->json(['error' => 'No file uploaded'], 400);
+        }
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+}
+
+}
