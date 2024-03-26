3869d1bfc1c67a438f7f4b407d1a0448085bae62
bmit-atm <martin.atanasov@espas.ch>
2024-03-26 22:51:56 +0100

    added some stuff, need to be fixed

--- config/auth.php
+++ config/auth.php
@@ -60,15 +60,23 @@ return [
     */
 
     'providers' => [
+        // ...
+    
         'users' => [
-            'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'driver' => 'ldap',
+            'model' => LdapRecord\Models\OpenLDAP\User::class,
+            'rules' => [],
+            'scopes' => [],
+            'database' => [
+                'model' => App\Models\User::class,
+                'sync_passwords' => false,
+                'sync_attributes' => [
+                    'name' => 'cn',
+                    'username' => 'uid',
+                    'email' => 'mail',
+                ],
+            ],
         ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
     ],
 
     /*
--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,11 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::post('/generate', 'App\Http\Controllers\SignatureController@generate');
-
-Route::get('/logs', 'App\Http\Controllers\LogController@index');
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
